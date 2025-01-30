e014dd91bc58b899e99afd4dcfa42acd017e761b
Thoufeek Hussain <th7.1998@gmail.com>
2025-01-30 23:52:03 +0530

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+
+def load_env():
+    load_dotenv()  # Load .env file if available
+    os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY', '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')
